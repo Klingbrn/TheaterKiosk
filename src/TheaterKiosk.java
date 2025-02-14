0f077f31fb67d95ce2e2fdb67df96a5ae9a25349
klingbrn <klingbrn@mail.uc.edu>
2025-02-14 18:08:15 -0500

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int age = 0;
+
+    System.out.println("Please enter your age: ");
+        
+        }
+    }
+}
