12b7b64a1c5b01e1222a2928d3fa5dbe03bcdd1b
klingbrn <klingbrn@mail.uc.edu>
2025-02-14 18:08:30 -0500

    Final Commit

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
-public class Main {
+public class TheaterKiosk {
     public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
-    int age = 0;
 
     System.out.println("Please enter your age: ");
-        
+    int age = in.nextInt();
+
+    if (age >= 21) {
+        System.out.println("You get a wristband!");
         }
     }
 }
